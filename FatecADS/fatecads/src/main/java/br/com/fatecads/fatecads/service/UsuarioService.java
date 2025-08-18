da8b78314359e9ab7f79e71d9abdc7cb2fbcdcf9
FrancianeRamos <francianeramos649@gmail.com>
2025-08-18 14:16:33 -0300

    feat: add methods to find all users and find user by ID in UsuarioService

--- FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/service/UsuarioService.java
+++ FatecADS/fatecads/src/main/java/br/com/fatecads/fatecads/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package br.com.fatecads.fatecads.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,14 @@ public class UsuarioService {
     public Usuario save (Usuario usuario) {
         usuario.setSenhaUsuario(passwordEncoder.encode(usuario.getSenhaUsuario()));
         return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Integer id) {
+        return usuarioRepository.findById(id).orElse(null);
     }
 
 }
